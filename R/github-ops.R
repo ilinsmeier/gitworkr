3595224f70bf76218a899e747407955580b4f6fc
ilinsmeier <19559173+ilinsmeier@users.noreply.github.com>
2023-09-02 18:40:26 -0400

    update description and examples in documentation for `gen_repo_from_template()` function

--- R/github-ops.R
+++ R/github-ops.R
@@ -1,34 +1,44 @@
 #' Create new repository from GitHub template repo
 #'
+#' Create a new GitHub repository from a template repo, clone the new repo to
+#' your local machine, create an RStudio project for the cloned repository, and
+#' open the RStudio project in a separate R session.
+#'
 #' @param repo_owner  github username and owner of the new github repository.
 #' @param repo_name   name of new the github repository being created.
 #' @param repo_descr  description for the new github repository.
-#' @param proj_dir    target path to clone repo and initialize RStudio project.
+#' @param proj_dir    target path for cloning repo and initializing RStudio
+#'   project.
 #' @param tmplt_owner username associated with github template repository.
 #' @param tmplt_repo  name of github template repository.
 #'
 #' @return list containing github API response object `gh_response` as well as
-#'   the local path to the created RStudio project
+#'   the local path to the new RStudio project
 #' @export
 #'
 #' @examples
 #' \dontrun{
 #'   ## create a new repo from a github template repository
 #'   gen_repo_from_template(
-#'     repo_owner = "",
-#'     repo_name = "",
-#'     repo_descr = "",
-#'     proj_dir = "",
-#'     tmplt_owner = "",
-#'     tmplt_repo = ""
+#'     repo_owner = "ilinsmeier",
+#'     repo_name = "new-github-repo-from-template",
+#'     repo_descr = "This repository was created from a github template repo.",
+#'     proj_dir = "~/projects",
+#'     tmplt_owner = "ilinsmeier",
+#'     tmplt_repo = "bg-labs-r-project-template"
 #'   )
 #' }
 #'
 #' @importFrom gh gh
 #' @importFrom usethis create_from_github
 #' @importFrom glue glue
-gen_repo_from_template <- function(repo_owner, repo_name, repo_descr, proj_dir,
-                                   tmplt_owner, tmplt_repo) {
+gen_repo_from_template <- function(repo_owner,
+                                   repo_name,
+                                   repo_descr = "",
+                                   proj_dir,
+                                   tmplt_owner,
+                                   tmplt_repo
+                                   ) {
 
   ## create repo from template using github api
   gh_response <- gh::gh("POST /repos/{template_owner}/{template_repo}/generate",
@@ -41,7 +51,6 @@ gen_repo_from_template <- function(repo_owner, repo_name, repo_descr, proj_dir,
          include_all_branches = FALSE,
          private = TRUE
   )
-  # print(gh_response)
 
   ## clone new github repo & open as RStudio project in a new session
   rproj_res <- usethis::create_from_github(
@@ -52,9 +61,6 @@ gen_repo_from_template <- function(repo_owner, repo_name, repo_descr, proj_dir,
     open = TRUE,
     protocol = "https"
   )
-  # print(rproj_res)
-
-  # return(gh_response)
 
   ## return github API response object & local path to created RStudio project
   list(
