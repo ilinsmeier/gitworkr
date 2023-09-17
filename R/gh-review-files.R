5d774e9f735a275afa2731a5e941ef60b42bac02
ilinsmeier <19559173+ilinsmeier@users.noreply.github.com>
2023-09-16 20:54:53 -0400

    wrap script code in function

--- R/gh-review-files.R
+++ R/gh-review-files.R
@@ -1,30 +1,31 @@
-PR_branch_name <- "test-PR-branch"
-review_file_comment <- "## file for review"
-PR_review_files <- c(
-  "README.md",
-  ".github/workflows/deploy_docs.yaml"
-)
-remote_repo <- gert::git_remote_list() |> print()
+gh_review_files <- function() {
+  PR_branch_name <- "test-PR-branch"
+  review_file_comment <- "## file for review"
+  PR_review_files <- c(
+    "README.md",
+    ".github/workflows/deploy_docs.yaml"
+  )
+  remote_repo <- gert::git_remote_list() |> print()
 
-## Create a branch
-gert::git_branch_create("test-feature", checkout = TRUE)
-initial_branch <- gert::git_branch() |> print()
-gert::git_branch_create(PR_branch_name, checkout = TRUE)
-stopifnot(identical(gert::git_branch(), PR_branch_name))
+  ## Create a branch
+  gert::git_branch_create("test-feature", checkout = TRUE)
+  initial_branch <- gert::git_branch() |> print()
+  gert::git_branch_create(PR_branch_name, checkout = TRUE)
+  stopifnot(identical(gert::git_branch(), PR_branch_name))
 
 
-PR_review_files_txt <- purrr::map(PR_review_files, readLines)
-PR_review_files_update <- purrr::map(PR_review_files_txt, ~ c(review_file_comment, "", .x))
-purrr::walk2(PR_review_files_update, PR_review_files, ~ writeLines(.x, .y))
+  PR_review_files_txt <- purrr::map(PR_review_files, readLines)
+  PR_review_files_update <- purrr::map(PR_review_files_txt, ~ c(review_file_comment, "", .x))
+  purrr::walk2(PR_review_files_update, PR_review_files, ~ writeLines(.x, .y))
 
-gert::git_commit_all(message = "setup review file branch")
+  gert::git_commit_all(message = "setup review file branch")
 
-gert::git_push(remote_repo$name)
+  gert::git_push(remote_repo$name)
 
-gert::git_status() |> print()
+  gert::git_status() |> print()
 
-usethis::pr_init(PR_branch_name)
-usethis::pr_push()
-
+  usethis::pr_init(PR_branch_name)
+  usethis::pr_push()
+}
 
 
